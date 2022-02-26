2f0a51b065fb2cb9d7cb1b5503afc9ff788ac8cc
wondonghwi <ehdehd1289@naver.com>
2022-02-26 18:37:33 +0900

    node -> POST / PUT 요청 text변경 validation 구현

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
+import { body } from 'express-validator';
 import * as tweetController from '../controller/tweet.js';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+
+const validateTweet = [body('text').trim().isLength({ min: 3 }).withMessage('text least 3 characters'), validate];
 
 // GET /tweets
 // GET /tweets?username=:username
@@ -10,9 +14,9 @@ router.get('/', tweetController.getTweets);
 // GET /tweets/:id
 router.get('/:id', tweetController.getTweet);
 // POST /tweets
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 // DELETE /tweets/:id
 router.delete('/:id', tweetController.deleteTweet);
 
