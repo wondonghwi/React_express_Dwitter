ca8d11847b61c316adf41efb83529a8755bc327f
wondonghwi <ehdehd1289@naver.com>
2022-03-11 23:11:57 +0900

    server - mysql2 설치 후 연결 작업진행

---
+++ server/db/database.js
@@ -0,0 +1,9 @@
+import mysql from 'mysql2';
+import { config } from '../config.js';
+
+export const pool = mysql.createPool({
+  host: config.db.host,
+  user: config.db.user,
+  database: config.db.database,
+  password: config.db.password,
+});
