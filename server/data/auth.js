57ba4cf4035998368c4edc4507a658ac3c3f7ba9
wondonghwi <ehdehd1289@naver.com>
2022-03-12 00:10:30 +0900

    server - my sql DB 연결 auth 작업

--- server/data/auth.js
+++ server/data/auth.js
@@ -1,32 +1,22 @@
-// abcd1234: $2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm
-let users = [
-  {
-    id: '1',
-    username: 'bob',
-    password: '$2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm',
-    name: 'Bob',
-    email: 'bob@gmail.com',
-    url: 'https://cdn.expcloud.co/life/uploads/2020/04/27135731/Fee-gentry-hed-shot-1.jpg',
-  },
-  {
-    id: '2',
-    username: 'donghwi',
-    password: '$2b$12$G9xf8SFq3oTEgdj7ozHQ/uhDOyeQcUEDU8tnOcvpvApuadr3nE5Vm',
-    name: 'Donghwi',
-    email: 'donghwi@gmail.com',
-  },
-];
+import { db } from '../db/database.js';
 
 export const findByUsername = async username => {
-  return users.find(user => user.username === username);
+  return db.execute('SELECT * FROM users WHERE username=?', [username]).then(result => result[0][0]);
 };
 
 export const findById = async id => {
-  return users.find(user => user.id === id);
+  return db.execute('SELECT * FROM users WHERE id=?', [id]).then(result => result[0][0]);
 };
 
 export const createUser = async user => {
-  const created = { ...user, id: Date.now().toString() };
-  users.push(created);
-  return created.id;
+  const { username, password, name, email, url } = user;
+  return db
+    .execute('INSERT INTO users (username , password, name, email, url) VALUES (?,?,?,?,?)', [
+      username,
+      password,
+      name,
+      email,
+      url,
+    ])
+    .then(result => result[0].insertId);
 };
