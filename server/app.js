5adce2fac12dce17449232f8ea04e99ef4fcdcf0
wondonghwi <ehdehd1289@naver.com>
2022-03-12 10:45:37 +0900

    server - ORM sequelize 라이브러리 설치 후 sequelize 세팅

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ import tweetsRouter from './router/tweets.js';
 import authRouter from './router/auth.js';
 import { config } from './config.js';
 import { initSocket } from './connection/socket.js';
-import { db } from './db/database.js';
+import { db, sequelize } from './db/database.js';
 
 const app = express();
 
@@ -30,7 +30,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-db.getConnection().then(connection => console.log(connection));
-
-const server = app.listen(config.host.port);
-initSocket(server);
+sequelize.sync().then(() => {
+  const server = app.listen(config.host.port);
+  initSocket(server);
+});
